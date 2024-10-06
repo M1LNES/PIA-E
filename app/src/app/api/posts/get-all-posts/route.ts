6d8d4a9ee400dd47d1391bb852a325edee268355
Milan Janoch <milan.janoch@emplifi.io>
2024-10-06 22:47:38 +0200

    REmoved unsused import

--- app/src/app/api/posts/get-all-posts/route.ts
+++ app/src/app/api/posts/get-all-posts/route.ts
@@ -1,6 +1,5 @@
 import { sql } from '@vercel/postgres'
 import { NextResponse } from 'next/server'
-import { getServerSession } from 'next-auth'
 
 export const revalidate = 1
 export async function GET() {
